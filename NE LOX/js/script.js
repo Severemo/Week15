8a30c12bc489cbbafb38aba341ac43b2d3ded7b2
Severemo <34189094+Severemo@users.noreply.github.com>
2018-03-31 13:12:25 +0300

    DOWNSKE ZAVDANNYA

---
+++ NE LOX/js/script.js
@@ -0,0 +1,23 @@
+function down(){
+function pow(n, x) {
+	var result = x;
+for (var i = 1; i < n; i++){
+	result *=x;
+}
+return result;
+}
+
+var x = prompt("x?");
+var n = prompt("n?");
+
+
+alert(pow(n, x));
+}
+
+// function zero(){
+//	alert ('Ihor pisun');
+// }
+ 
+
+
+
